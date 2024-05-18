944e3d86ee024932d053367d705bee8bb3c590aa
Tarcisio Oliveira <t@rcis.io>
2024-05-18 00:19:51 +0000

    update

--- utils/pubchem.py
+++ utils/pubchem.py
@@ -25,3 +25,37 @@ def download_compound_properties(CIDs : list, properties : list) -> list:
     response = response['PropertyTable']['Properties']
 
     return response
+
+
+def download_compound_images(CIDs : list, image_size : int, directory : str):
+    """
+    Downloads compound images from PubChem.
+
+    Parameters:
+        - CIDs       (list) : List of compound identifiers.
+        - image_size (int)  : Size of images.
+        - directory  (str)  : Directory where the images will be saved.
+
+    Returns
+        - None
+
+    Dependencies:
+        - requests
+    """
+
+    import os
+    import requests
+
+    for CID in CIDs:
+
+        url = f'https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/cid/{ CID }/PNG?image_size={ image_size }x{ image_size }'
+
+        response = requests.get(url)
+        response = response.content
+
+        image_file = f'{ CID }_PubChem.png'
+        image_file = os.path.join(directory, image_file)
+
+        with open(image_file, 'wb') as file:
+
+            file.write(response)
