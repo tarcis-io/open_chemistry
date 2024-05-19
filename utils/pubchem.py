e10542c4d3ed4ca9b148f1cb4f6e57bbbe1651d9
Tarcisio Oliveira <t@rcis.io>
2024-05-19 00:18:12 +0000

    update

--- utils/pubchem.py
+++ utils/pubchem.py
@@ -60,3 +60,47 @@ def download_compound_images(CIDs : list, image_size : int, directory : str):
         with open(image_file, 'wb') as file:
 
             file.write(response)
+
+
+def plot_compound_images(title : str, directory : str, compounds : list):
+    """
+    Plots the images of the chemical compounds downloaded from PubChem.
+
+    Parameters:
+        - title     (str)  : Title of the image.
+        - directory (str)  : Directory where the images are saved.
+        - compounds (list) : List of the compound dictionaries with the keys 'CID' and 'Title'.
+
+    Returns:
+        - None
+
+    Dependencies:
+        - math
+        - matplotlib
+        - os
+    """
+
+    import math
+    import matplotlib.pyplot
+    import os
+
+    fig_cols = 5
+    fig_rows = math.ceil(len(compounds) / fig_cols)
+
+    fig, axs = matplotlib.pyplot.subplots(fig_rows, fig_cols, figsize = (16, 16), constrained_layout = True)
+    fig.suptitle(title, fontsize = 16)
+
+    axs = axs.flatten()
+    for ax in axs: ax.axis('off')
+
+    for index, compound in enumerate(compounds):
+
+        CID   = compound['CID']
+        Title = compound['Title']
+
+        image_file = f'{ CID }.png'
+        image_file = os.path.join(directory, image_file)
+
+        ax = axs[index]
+        ax.set_title(Title)
+        ax.imshow(matplotlib.pyplot.imread(image_file))
