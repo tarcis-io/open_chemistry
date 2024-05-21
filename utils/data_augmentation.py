3c4e6e1d89a2a98ce0f133c5d88bb0d983dddb46
Tarcisio Oliveira <t@rcis.io>
2024-05-21 02:38:47 +0000

    update

--- utils/data_augmentation.py
+++ utils/data_augmentation.py
@@ -25,9 +25,65 @@ def create_images_with_transpositions(image_file : str):
     ]
 
     image      = Image.open(image_file)
-    image_file = image.filename.rsplit('.', 1)[0]
+    image_file = image_file.rsplit('.', 1)[0]
 
     for transposition in transpositions:
 
         image_with_transposition = image.transpose(transposition)
         image_with_transposition.save('{}_with_transposition_{}.png'.format(image_file, transposition))
+
+
+def plot_images_with_transpositions(title : str, image_file : str):
+    """
+    Plots the given image and its transposed images.
+
+    Parameters:
+        - title      (str) : Title of the image.
+        - image_file (str) : Image file that has transposed images.
+
+    Returns:
+        - None
+
+    Dependencies:
+        - math
+        - matplotlib
+        - pillow
+    """
+
+    import math
+    import matplotlib.pyplot
+    from PIL import Image
+
+    transpositions = [
+        Image.FLIP_LEFT_RIGHT,
+        Image.FLIP_TOP_BOTTOM,
+        Image.ROTATE_90,
+        Image.ROTATE_180,
+        Image.ROTATE_270,
+        Image.TRANSPOSE,
+        Image.TRANSVERSE
+    ]
+
+    files = [
+        image_file
+    ]
+
+    image_file = image_file.rsplit('.', 1)[0]
+
+    for transposition in transpositions:
+
+        files.append('{}_with_transposition_{}.png'.format(image_file, transposition))
+
+    fig_cols = 5
+    fig_rows = math.ceil(len(files) / fig_cols)
+
+    fig, axs = matplotlib.pyplot.subplots(fig_rows, fig_cols, figsize = (16, 16), constrained_layout = True)
+    fig.suptitle(title, fontsize = 16)
+
+    axs = axs.flatten()
+    for ax in axs: ax.axis('off')
+
+    for index, file in enumerate(files):
+
+        ax = axs[index]
+        ax.imshow(matplotlib.pyplot.imread(file))
