e60f33faf74f41999a16f84a1f87e9dab79727c4
Tarcisio Oliveira <t@rcis.io>
2024-05-21 00:08:15 +0000

    update

---
+++ utils/data_augmentation.py
@@ -0,0 +1,33 @@
+def create_images_with_transpositions(image_file : str):
+    """
+    Creates images with transpositions from the provided image file.
+
+    Parameters:
+        - image_file (str) : Image file in which the transpositions will be created.
+
+    Returns:
+        - None
+
+    Dependencies:
+        - pillow
+    """
+
+    from PIL import Image
+
+    transpositions = [
+        Image.FLIP_LEFT_RIGHT,
+        Image.FLIP_TOP_BOTTOM,
+        Image.ROTATE_90,
+        Image.ROTATE_180,
+        Image.ROTATE_270,
+        Image.TRANSPOSE,
+        Image.TRANSVERSE
+    ]
+
+    image      = Image.open(image_file)
+    image_file = image.filename.rsplit('.', 1)[0]
+
+    for transposition in transpositions:
+
+        image_with_transposition = image.transpose(transposition)
+        image_with_transposition.save('{}_with_transposition_{}.png'.format(image_file, transposition))
